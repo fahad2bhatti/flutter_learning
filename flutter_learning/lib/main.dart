cdb171867d0d0e9c8b2a8c59a1084efcc55a1512
fahad2bhatti <fahaddragon262@gmail.com>
2026-06-30 16:14:28 +0500

    learning start  aritmatic opeartions Done

---
+++ flutter_learning/lib/main.dart
@@ -0,0 +1,24 @@
+void main() {
+  /* String color = "blue";
+  print("Fvrt color is $color");
+
+  String food = "pizza";
+  print("Fvrt food is $food");
+
+  int number = 10;
+
+  print("Fvrt number is $number");
+
+  bool likeflutter = true;
+  print(" I like flutter? $likeflutter");*/
+
+  int a = 25;
+  int b = 4;
+
+  print(a + b);
+  print(a - b);
+    print(a * b);
+    print(a / b);
+      print(a % b);
+
+}
